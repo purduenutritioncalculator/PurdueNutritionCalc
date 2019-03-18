404b6704c08904882c20c93a8fd7786838ec96eb
Anderson <andersondavid2000@gmail.com>
2019-03-18 18:45:10 -0400

    initial commit

---
+++ NutritionCalc/NutritionCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NutritionCalc
+//
+//  Created by Anderson David on 3/18/19.
+//  Copyright © 2019 Anderson David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
