7eaca2ca57ff7884a8506d07b570721afa7459a9
gyolasan <yolasan4g@gmail.com>
2019-03-25 18:16:20 -0400

    Added the nutrition view for individual items

---
+++ NutritionCalc/NutritionCalc/ItemNutritionCell.swift
@@ -0,0 +1,27 @@
+//
+//  ItemNutritionCell.swift
+//  NutritionCalc
+//
+//  Created by Grant Yolasan on 3/25/19.
+//  Copyright © 2019 Anderson David. All rights reserved.
+//
+
+import UIKit
+
+class ItemNutritionCell: UITableViewCell {
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    @IBOutlet weak var quantityLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ NutritionCalc/NutritionCalc/ItemNutritionViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ItemNutritionViewController.swift
+//  NutritionCalc
+//
+//  Created by Grant Yolasan on 3/25/19.
+//  Copyright © 2019 Anderson David. All rights reserved.
+//
+
+import UIKit
+
+class ItemNutritionViewController: UIViewController {
+
+    @IBOutlet weak var itemNutritionTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
