33e1260aea2a765585fab2266ef995e8cb3a963b
gyolasan <yolasan4g@gmail.com>
2019-04-01 18:15:34 -0400

    added meal struct

---
+++ NutritionCalc/NutritionCalc/mealStruct.swift
@@ -0,0 +1,16 @@
+//
+//  mealStruct.swift
+//  NutritionCalc
+//
+//  Created by Grant Yolasan on 4/1/19.
+//  Copyright © 2019 Anderson David. All rights reserved.
+//
+
+import Foundation
+
+struct meal {
+    var calories:Int
+    var protein:Int
+    var fat:Int
+    var carbs:Int
+}
