39c4f9fc3e122720449376f9563315a98c2b2bc0
Zach <zach@pal-nat186-59-72.itap.purdue.edu>
2019-04-10 18:53:27 -0400

    edit profile

---
+++ NutritionCalc/NutritionCalc/ViewControllers/EditProfileViewController.swift
@@ -0,0 +1,33 @@
+//
+//  EditProfileViewController.swift
+//  NutritionCalc
+//
+//  Created by Zach on 4/10/19.
+//  Copyright © 2019 Anderson David. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func onCancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
