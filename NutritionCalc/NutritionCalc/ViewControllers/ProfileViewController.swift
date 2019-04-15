96897647e85d3a940a268d13419c410b0c5aad43
gyolasan <yolasan4g@gmail.com>
2019-04-15 18:30:37 -0400

    core data on the profile vc

--- NutritionCalc/NutritionCalc/ViewControllers/ProfileViewController.swift
+++ NutritionCalc/NutritionCalc/ViewControllers/ProfileViewController.swift
@@ -7,16 +7,47 @@
 //
 
 import UIKit
+import CoreData
 
 class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var todayCals: UILabel!
+    
+    var myMealHistory = [MealModel]()
+    var todayMeals = [MealModel]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        todayCals.text = "25"
 
+        let fetchRequest: NSFetchRequest<MealModel> = MealModel.fetchRequest()
+        
+        do {
+            let meals = try PersistenceService.context.fetch(fetchRequest)
+            self.myMealHistory = meals
+        } catch {
+            print("error")
+        }
+        print("profile array length: \(myMealHistory.count)")
         // Do any additional setup after loading the view.
+        getTodaysMeal()
     }
     
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("profile array length: \(myMealHistory.count)")
+    }
+    
+    func getTodaysMeal() {
+        let today = NSDate()
+        for meal in myMealHistory {
+            let todayy = Calendar.current.isDateInToday(meal.date as! Date)
+            if todayy {
+                todayMeals.append(meal)
+            }
+        }
+        print("first meal cals = \(self.myMealHistory[0].calories)")
+    }
     /*
     // MARK: - Navigation
 
